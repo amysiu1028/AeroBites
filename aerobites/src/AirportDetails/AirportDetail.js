3cd27f168dae96b1a09d27515199575e0c2acaf4
Emalee Poellot <emalee.poellot@gmail.com>
2024-01-01 13:32:16 -0700

    feat/Add more fetches for terminals and business

--- aerobites/src/AirportDetails/AirportDetail.js
+++ aerobites/src/AirportDetails/AirportDetail.js
@@ -1,18 +1,42 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import { useParams, Link } from 'react-router-dom';
+import { getTerminals, getBusinesses } from '../ApiCalls/ApiCalls';
 
 export default function AirportDetails({ airports, toggleFavorite }) {
     // get airport name from the URL using useParams
-    const { airportName } = useParams();
-    const decodedName = decodeURIComponent(airportName);
-    const airport = airports.find(a => a.name === decodedName);
+    const { airportIdString } = useParams()
+    const preairportId = useParams().Id
+    const airportId = parseInt(preairportId)
+    console.log(airportId, 'airportId')
+    // const decodedName = decodeURIComponent(airportId);
+    const airport = airports.find(a => a.airport_id === airportIdString);
+    const [terminals, setTerminals ] = useState([]);
 
+    
+    useEffect(() => {
+        getTerminals()
+        .then(data => {
+            // console.log(data)
+            setTerminals(data.filter(terminal => {
+                // console.log(terminal, 'terminal')
+                // console.log(terminal.airport_id)
+               return  terminal.airport_id === airportId}
+                ))
+                getBusinesses()
+                // console.log(terminals, 'outside useeffect')
+                // console.log(airports)
+            })
+            .catch(error => {
+                // console.log(error);
+            })
+        },[])
+    
     // airport is not found
     if (!airport) {
+        { console.log(terminals, 'terminals in return statement') }
         return <div>Airport not found</div>;
     }
-
     return (
         <div className='airport-details'>
             <Link to="/favorites">Show Favorites</Link>
@@ -22,22 +46,14 @@ export default function AirportDetails({ airports, toggleFavorite }) {
                 {airport.isFavorite ? 'Favorite ❤️' : 'Favorite 🤍'}
             </button>
             {/* display terminal and restaurants*/}
-            {airport.terminals.map((terminal) => {
-                // get the terminal name
-                const terminalName = Object.keys(terminal)[0];
-                return (
-                    <div>
-                        <h3>{terminalName}</h3>
-                        <ul>
-                            {/* map over the restaurants in each terminal */}
-                            {terminal[terminalName].map((restaurant, index) => (
-                                // need to create unique keys at some point
-                                <li id={terminalName} key={terminalName}>{restaurant}</li>
-                            ))}
-                        </ul>
-                    </div>
-                );
-            })}
+        {terminals.map(terminal => {
+            // get the terminal name
+            return (
+                <div>
+                    <h3>{terminal.terminalName}</h3>
+                </div>
+            );
+        })}
         </div>
     );
 }
@@ -50,8 +66,9 @@ AirportDetails.propTypes = {
             isFavorite: PropTypes.bool.isRequired,
             terminals: PropTypes.arrayOf(
                 PropTypes.object
-            ) 
-        })
-    ).isRequired,
-    toggleFavorite: PropTypes.func.isRequired
-};
+                ) 
+            })
+            ).isRequired,
+            toggleFavorite: PropTypes.func.isRequired
+        };
+    
--- aerobites/src/ApiCalls/ApiCalls.js
+++ aerobites/src/ApiCalls/ApiCalls.js
@@ -1,13 +1,35 @@
-
-
-
-export default function getAirports() {
-    fetch('http://localhost:3000')
+function getAirports() {
+    return fetch('http://localhost:8080')
         .then(response => {
             if(!response.ok) {
                 // throw new Error (`${error}: Failed to fetch data`)
                 console.log('error')
             }
+            // console.log(response.json())
             return response.json();
         })
 }
+
+function getTerminals() {
+    return fetch('http://localhost:8080/terminals')
+        .then(response => {
+            if(!response.ok) {
+                console.log('error')
+            }
+            return response.json();
+        })
+}
+
+function getBusinesses() {
+    return fetch('http://localhost:8080/businesses')
+    .then(response => {
+        if(!response.ok) {
+            console.log('error')
+        }
+        return response.json();
+    })
+}
+
+
+
+export { getAirports, getTerminals, getBusinesses }
