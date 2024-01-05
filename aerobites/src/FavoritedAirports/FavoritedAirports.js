cefdfbbe99cf0bcee12f952d9d53b1ba20bd1427
Emalee Poellot <emalee.poellot@gmail.com>
2024-01-04 18:02:10 -0700

    fix/Delete test useEffect and clean up code

--- aerobites/src/FavoritedAirports/FavoritedAirports.js
+++ aerobites/src/FavoritedAirports/FavoritedAirports.js
@@ -33,7 +33,6 @@ const FavoriteAirports = ({ getFavoriteAirports, toggleFavorite }) => {
             ) : (
                 <p>No favorites yet</p>
             )}
-            </div>
         </div>
        <div ref={container} id="animation-container">
             </div>
